7206ebfdafa3139af8f5023f342017e24a5f8f4c
Kevin Longe <273747162+kvlonge@users.noreply.github.com>
2026-09-10 12:09:40 +0100

    chore: adopt native Fensu Rust analysis

--- crates/sqlbuild-structure-checker/src/main.rs
+++
@@ -1,9 +0,0 @@
-//! Command adapter for the structure checker.
-
-use std::process::ExitCode;
-
-use fensu_structure_checker::command::main::run_checker::run_checker;
-
-fn main() -> ExitCode {
-    run_checker()
-}
